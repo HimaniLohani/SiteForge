5904868962bf72c06be2d97031255c04d0450e49
Himani Lohani <himanilohani68@gmail.com>
2026-06-08 23:41:36 +0530

    Added backend structure (controllers, models, database)

---
+++ backend/controllers/authController.js
@@ -0,0 +1,40 @@
+
+import { User } from '../models/user.js';
+import jwt from 'jsonwebtoken';
+export const googleAuth = async(req, res) => {
+    try {
+        const { name, email, avatar } = req.body;
+        let user = await User.findOne({ email });
+        if (!user) {
+            user=await User.create({ name, email, avatar });
+        }
+        const token=jwt.sign({ id: user._id }, process.env.SECRET_KEY, { expiresIn: '7d' });
+        res.cookie('token', token,
+            {httpOnly:true, secure:false,sameSite:'strict',maxAge:7*24*60*60*1000}
+        ); 
+        return res.status(200).json(user);
+
+    }
+
+    catch (error) {
+        return res.status(500).json({ 
+            success: false,
+            message:error.message
+        });
+    }
+};
+export const logout = async(req, res) => {
+    try{
+        res.clearCookie("token")
+        return res.status(200).json({ 
+            
+            message: "Logged out successfully"
+        });
+    }
+    catch(error){
+        return res.status(500).json({ 
+            success: false,
+            message:error.message
+        });
+    }  
+};     
